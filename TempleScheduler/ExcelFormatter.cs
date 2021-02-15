734689a5b93a79477a1bfd296b9c0d5f673e167e
Tacobell <franklinlantigua90@gmail.com>
2021-02-15 14:00:27 -0500

    Changes -Added a class for the excel formatter

---
+++ TempleScheduler/ExcelFormatter.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace TempleScheduler
+{
+   public class ExcelFormatter
+    {
+        public string path;
+        public ExcelFormatter(string path)
+        {
+            this.path = path;
+        }
+
+        public void FileNames()
+        {
+            string[] fileEntries = Directory.GetFiles(this.path);
+            Console.WriteLine(fileEntries[0]);
+        }
+
+
+    }
+}
